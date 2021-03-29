62bdaa8d09ae2ed057f539366b6664cd887cfc61
Lionel Robert <lionelr@umich.edu>
2021-03-29 09:04:41 -0400

    update adupdateview and adcreateview

--- mysite/ads/views.py
+++ mysite/ads/views.py
@@ -1,5 +1,10 @@
+from django.views import View
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Ad
 from .owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+from .forms import CreateForm
 
 
 class AdListView(OwnerListView):
@@ -12,14 +17,59 @@ class AdDetailView(OwnerDetailView):
     model = Ad
 
 
-class AdCreateView(OwnerCreateView):
-    model = Ad
-    fields = ['title', 'text', 'price']
+# class AdCreateView(OwnerCreateView):
+#     model = Ad
+#     fields = ['title', 'text', 'price']
+
+class AdCreateView(LoginRequiredMixin, View):
+    template_name = 'ads/form.html'
+    success_url = reverse_lazy('ads:all')
+
+    def get(self, request, pk=None):
+        form = CreateForm()
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request, pk=None):
+        form = CreateForm(request.POST, request.FILES or None)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
+
+        # Add owner to the model before saving
+        pic = form.save(commit=False)
+        pic.owner = self.request.user
+        pic.save()
+        return redirect(self.success_url)
 
 
-class AdUpdateView(OwnerUpdateView):
-    model = Ad
-    fields = ['title', 'text', 'price']
+# class AdUpdateView(OwnerUpdateView):
+#     model = Ad
+#     fields = ['title', 'text', 'price']
+
+class AdUpdateView(LoginRequiredMixin, View):
+    template_name = 'ads/form.html'
+    success_url = reverse_lazy('ads:all')
+
+    def get(self, request, pk):
+        ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(instance=ad)
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request, pk=None):
+        ad = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(request.POST, request.FILES or None, instance=ad)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
+
+        ad = form.save(commit=False)
+        ad.save()
+
+        return redirect(self.success_url)
 
 
 class AdDeleteView(OwnerDeleteView):
