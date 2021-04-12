ffee2fb6f60c64c6f33025689cc1a744a4ff02fb
Lionel Robert <lionelr@umich.edu>
2021-04-12 12:15:44 -0400

    search functionality added

--- mysite/ads/views.py
+++ mysite/ads/views.py
@@ -6,6 +6,9 @@ from django.db.utils import IntegrityError
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from django.db.models import Q
+from .utils import dump_queries
 from .models import Ad, Comment, Fav
 from .owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 from .forms import CreateForm, CommentForm
@@ -18,14 +21,32 @@ class AdListView(OwnerListView):
 
     def get(self, request):
         ad_list = Ad.objects.all()
+        strval = request.GET.get("search", False)
+        if strval:
+            # Simple title-only search
+            # objects = Ad.objects.filter(title__contains=strval).select_related().order_by('-updated_at')[:10]
+
+            # Multi-field search
+            # __icontains for case-insensitive search
+            query = Q(title__icontains=strval)
+            query.add(Q(text__icontains=strval), Q.OR)
+            ad_list = Ad.objects.filter(
+                query).select_related().order_by('-updated_at')[:10]
+        else:
+            objects = Ad.objects.all().order_by('-updated_at')[:10]
+        # Augment the post_list
+        for ad in ad_list:
+            ad.natural_updated = naturaltime(ad.updated_at)
         favorites = []
         if request.user.is_authenticated:
             # rows = [{'id': 2}, {'id': 4} ... ]  (A list of rows)
             rows = request.user.favorite_ads.values('id')
             # favorites = [2, 4, ...] using list comprehension
             favorites = [row['id'] for row in rows]
-        ctx = {'ad_list': ad_list, 'favorites': favorites}
-        return render(request, self.template_name, ctx)
+        ctx = {'ad_list': ad_list, 'favorites': favorites,'search': strval}
+        retval = render(request, self.template_name, ctx)
+        dump_queries()
+        return retval
 
 
 class AdDetailView(OwnerDetailView):
